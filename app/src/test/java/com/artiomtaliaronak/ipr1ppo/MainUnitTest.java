afc5ccb829b58cb2ed98f0be9fcb4603f10bded7
artiomtaliaronak <artiomtaliaronak@gmail.com>
2023-01-23 13:11:18 +0300

    added unittests for database, login and user class

--- app/src/test/java/com/artiomtaliaronak/ipr1ppo/MainUnitTest.java
+++ app/src/test/java/com/artiomtaliaronak/ipr1ppo/MainUnitTest.java
@@ -1,41 +1,43 @@
 package com.artiomtaliaronak.ipr1ppo;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 
-import android.view.View;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 @RunWith(JUnit4.class)
 public class MainUnitTest {
 
-    @Mock
-    View view;
-
-    @Mock
-    public MainActivity mainActivity;
-
-
-    @Before
-    public void setUp() throws Exception {
-        mainActivity = Mockito.mock(MainActivity.class);
+    @Test
+    public void startTest(){
+        MainActivity mainActivity = Mockito.mock(MainActivity.class);
+        mainActivity.checkUsername();
+        verify(mainActivity).checkUsername();
     }
 
     @Test
-    public void startGame_isCorrect() throws Exception {
-        mainActivity.startGame(view);
-        verify(mainActivity).startGame(view);
+    public void userCreationTest(){
+        User user = new User(-1,"test", 1);
+        assertEquals(-1, user.getId());
+        assertEquals("test", user.getUsername());
+        assertEquals(1, user.getScore());
     }
 
-    @After
-    public void tearDown() throws Exception {
-        mainActivity = null;
-        view = null;
+    @Test
+    public void readAllDataTest(){
+        DatabaseHelper databaseHelper = Mockito.mock(DatabaseHelper.class);
+        databaseHelper.readData();
+        verify(databaseHelper).readData();
     }
+
+    @Test
+    public void updateDatabaseTest(){
+        DatabaseHelper databaseHelper = Mockito.mock(DatabaseHelper.class);
+        databaseHelper.updateDatabase(10, "testupdate", 1);
+        verify(databaseHelper).updateDatabase(10, "testupdate",1);
+    }
+
 }
